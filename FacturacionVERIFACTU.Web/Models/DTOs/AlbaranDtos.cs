a968d3ef675e5a4fcb6490f404be12094e53c840
Skarecito <151457039+Skarecito@users.noreply.github.com>
2026-02-15 09:28:40 +0100

    Final limpieza fiscal

--- FacturacionVERIFACTU.Web/Models/DTOs/AlbaranDtos.cs
+++ FacturacionVERIFACTU.Web/Models/DTOs/AlbaranDtos.cs
@@ -5,8 +5,27 @@ namespace FacturacionVERIFACTU.Web.Models.DTOs;
 public class AlbaranResponseDto
 {
     public int Id { get; set; }
-    public string? Numero { get; set; }
-    public string? Estado { get; set; }
+    public int TenantId {  get; set; }
+    public string NumeroAlbaran { get; set; } = string.Empty;
+    public int SerieId {  get; set; }
+    public int Numero {  get; set; }
+    public int Ejercicio { get; set; }
+    public DateTime FechaEmision {  get; set; }
+    public DateTime? FechaEntrega {  get; set; }
+    public int ClienteId { get; set;}
+    public string? ClienteNombre { get; set; }
+    public string Estado {  get; set; }
+    public decimal BaseImponible {  get; set; }
+    public decimal TotalIVA {  get; set; }
+    public decimal Total {  get; set; }
+    public string? Observaciones {  get; set; }
+    public decimal TotalRecargo {  get; set; }
+    public decimal PorcentajeRetencion { get; set; }
+    public decimal CuotaRetencion { get; set; }
+    public decimal TotalConRetencion { get; set; }
+    public List<lineaAlbaranResponseDto> Lineas { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public DateTime? FechaModificacion { get; set; }
 }
 
 public class ConvertirPresupuestoAAlbaranDto
